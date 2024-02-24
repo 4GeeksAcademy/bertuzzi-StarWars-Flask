1070435ec3b3391e9661792e7c192eef3cce6112
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-02-24 11:49:29 +0000

    update models and create endpoints

--- src/app.py
+++ src/app.py
@@ -55,8 +55,43 @@ def handle_users():
         response_body['message'] = 'GET method invoked'
         return response_body,200
     elif request.method == 'POST':
+        # First we need to receive the data from the front to put in the object (we need to use .json method to make it readable from the backend)
+        data = request.json
+        ## data is an object that represents what has been sent on the request (in this case email,password)
+        # Need to get the body from the frontend
+        # Create an instance of the class Users (here we create a user with fixed values that we define by ourselves)
+        # Here we now complete the new instance values with the key-value pairs of the request body
+        user = Users(email = data['email'],
+                    password = data['password'],
+                    is_active = True)
+        db.session.add(user)
+        # Upon creating a user and committing the change, in this case we have an autoincremental id, so we do not need to create an id on our side
+        db.session.commit()
+        # Need to add .serialialize() because we need to return a value that is readable to the human eye (the serialize method is defined within the class in the models)
+        response_body['result_user'] = user.serialize()
         response_body['message'] = 'POST method invoked'
         return response_body,200
+
+# In this case we need to get a single user and therefore we need to modifiy the route to include the id that we send
+@app.route('/user/<int:id>',methods=['GET'])
+def handle_user(id):
+    response_body = {}
+    if request.method == 'GET':
+        print(id)
+        response_body['message'] = 'GET request of user ' + str(id)
+        return response_body, 200
+    if request.method == 'PUT':
+        print(id)
+        response_body['message'] = 'GET request of user ' + str(id)
+        return response_body, 200
+    if request.method == 'DELETE':
+        # Logically speaking, if the user is DELETED we do not need to delete it, but we need to put the user as inactive otherwise there are gonna be inconsistencies with the database tables (is_active=False)
+        print(id)
+        response_body['message'] = 'GET request of user ' + str(id)
+        return response_body, 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
