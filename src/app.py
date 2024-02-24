0edceff6cb1898eecb5cb41381add471f4f64b1a
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-02-24 11:03:43 +0000

    update code

--- src/app.py
+++ src/app.py
@@ -8,19 +8,19 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Users
 #from models import Person
 
+# Flask instance
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+#Configure db (database)
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -36,14 +36,27 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+@app.route('/users', methods=['GET','POST'])
+def handle_users():
+    response_body = results = {}
+    if request.method == 'GET':
+        # Here we add logic to consult database db and return all the users from the database
+        # .scalars() returns an array of SQLAlchemy objects, whilst .scalar() returns a dictionary
+        users = db.session.execute(db.select(Users)).scalars()
+        results['users'] = [row.serialize() for row in users]
+        response_body['result_users'] = results['users']
+        response_body['message'] = 'GET method invoked'
+        return response_body,200
+    elif request.method == 'POST':
+        response_body['message'] = 'POST method invoked'
+        return response_body,200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,13 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    # Def allows us to represent 
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -16,4 +19,6 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            # This is what we send to the front as a dictionary
+            # The serialize method allows us give back the specific information we want to return to the front, for instance by means of an endpoint
         }
